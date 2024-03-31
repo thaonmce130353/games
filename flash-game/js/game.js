3d04e328bc9de3882c4e77252f9f85b4713e6e57
Thao Nguyen <nguyenminhthaobt1@gmail.com>
2024-03-31 10:44:41 +0700

    Update flash games

---
+++ flash-game/js/game.js
@@ -0,0 +1,13 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const gameURL = urlParams.get('game');
+window.RufflePlayer = window.RufflePlayer || {};
+window.addEventListener("load", (event) => {
+  const ruffle = window.RufflePlayer.newest();
+  const player = ruffle.createPlayer();
+  player.style.width = "900px";
+  player.style.height = "700px";
+  const container = document.getElementById("game");
+  container.appendChild(player);
+  player.load(gameURL + ".swf");
+});
